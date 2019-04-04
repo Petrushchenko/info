0fabb29a368b54cd9b50359efb181b6b647d975c
Petrushchenko <petrushchenko.l@ukr.net>
2019-04-04 23:23:52 +0300

    some styles for table

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -12,30 +12,41 @@
             </thead>
             <tbody>
               <tr v-for="person in persons">
-                <td>{{ person.id }}</td>
                 <td class="name">
                   <div v-if="!person.editable" 
                        @click="makeEditable( person )"
                        v-html="highlight(person.name, wordToHighlight)"
-                  >    
-                  </div>
+                  ></div>
                   <textarea v-else 
                             @keypress.enter.prevent="changeData(person)"
                             v-model="person.name"
                             autofocus
-                            @mouseout="makeUneditable(person)"
-                    >
-                    {{person.name}}</textarea>
-                    
+                    ></textarea> 
                 </td>
 
-                <td class="email" v-html="highlight(person.email, wordToHighlight)">
-     
-    
+                <td class="email">
+                  <div v-if="!person.editable" 
+                       @click="makeEditable( person )"
+                       v-html="highlight(person.email, wordToHighlight)"
+                  ></div>
+                  <textarea v-else 
+                            @keypress.enter.prevent="changeData(person)"
+                            v-model="person.email"
+                            autofocus
+                    ></textarea>
                 </td>
-                <td class="city" v-html="highlight(person.city, wordToHighlight)"></td>
+                <td class="city">
+                  <div v-if="!person.editable" 
+                       @click="makeEditable( person )"
+                       v-html="highlight(person.city, wordToHighlight)"
+                  ></div>
+                  <textarea v-else 
+                            @keypress.enter.prevent="changeData(person)"
+                            v-model="person.city"
+                            autofocus
+                    ></textarea>
+                </td>
                 <td class="phone">{{person.phone}}</textarea></td>
-                <td>{{ person.editable }}</td>
               </tr>
             </tbody>
         </table> 
@@ -55,7 +66,7 @@ export default {
   computed: {
     tableFields() {
       let keys = Object.keys(this.persons[0])
-      return keys.filter(key => key !== "funds") //key !== "id" && 
+      return keys.filter(key => key !== "id" && key !== "funds" && key !=="editable")
     }
   },
   methods: {
@@ -65,10 +76,6 @@ export default {
     },
     makeEditable(person) {
       this.persons.forEach( item => item.id === person.id ? item.editable = true : null);
-    },
-    makeUneditable(person) {
-       this.persons.forEach( item => item.id === person.id ? item.editable = false : null);
-      
     },
     changeData (person) {
       this.persons.forEach( item => item.id === person.id ? item.editable = false : null);
@@ -86,6 +93,9 @@ export default {
     width: 80%;
     margin: 0 auto;
     border-color: #A8698A;
+    tr:hover {
+      background-color: #f2f2f2;
+    }
     td {
       cursor: pointer;
       textarea {
@@ -95,19 +105,12 @@ export default {
         width: 100%;
         height: 100%;
         border:none;
+        background-color: #f2f2f2;
+
       }
       div {
         padding: 10px 0;
       }
     }
-  }
-  .popup {
-    position: absolute;
-    padding: 10px;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
-    width: 200px;
-    background-color: #ae7272;
   }
 </style>
